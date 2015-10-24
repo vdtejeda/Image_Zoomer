82524ae06c0150554e552581dcda4e4dc8195b66
Volney Tejeda <volneytejeda10@gmail.com>
2015-10-24 15:07:34 -0400

    add custom helper

--- lib/image_zoomer.rb
+++ lib/image_zoomer.rb
@@ -1,5 +1,14 @@
 require "image_zoomer/version"
 require "image_zoomer/engine"
+require "image_zoomer/zoom_image_tag"
+
+module ActionView
+  module Helpers
+    module AssetTagHelper
+      include ImageZoomer
+    end
+  end
+end
 
 module ImageZoomer
   # Your code goes here...
