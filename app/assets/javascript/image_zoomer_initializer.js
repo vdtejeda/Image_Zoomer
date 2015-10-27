ac7de0ae1cf71a692848962704e52cbf9d7ab322
Volney Tejeda <volneytejeda10@gmail.com>
2015-10-27 17:27:34 -0400

    allow changes to configuration

--- app/assets/javascript/image_zoomer_initializer.js
+++ app/assets/javascript/image_zoomer_initializer.js
@@ -1,5 +1,8 @@
 $(function(){
+  if (typeof __ImageZoomer === "undefined"){
+    __ImageZoomer = {options: {}};
+  }
   $('.image_zoomer').each(function() {
-    $(this).image_zoomer();
+    $(this).image_zoomer(__ImageZoomer.options);
   });
 });
--- lib/image_zoomer/configuration.rb
+++ lib/image_zoomer/configuration.rb
@@ -7,5 +7,10 @@ module ImageZoomer
       @height = 90
       @zoom_level = 1.5
     end
+
+    # Override default options with the provided ones in block
+    def set_options(&block)
+      block.yield(self)
+    end
   end
 end
