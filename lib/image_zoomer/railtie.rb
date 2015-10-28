0692003d635e4b52ff522a5a53397c610359be6d
Volney Tejeda <volneytejeda10@gmail.com>
2015-10-28 10:33:17 -0400

    fix setting change by clearing assets cache

--- lib/image_zoomer/railtie.rb
+++ lib/image_zoomer/railtie.rb
@@ -1,6 +1,8 @@
 class Railtie < Rails::Railtie
   initializer "image_zoomer.configuration" do |app|
     app.config.image_zoomer = ImageZoomer::Configuration.new
+    # Clear Cache
+    FileUtils.rm_rf("#{Rails.application.root}/tmp/cache/assets") if Rails.env.development?
   end
 
   initializer "image_zoomer.initialize_zoom_image_helper" do |app|
